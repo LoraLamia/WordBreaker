cc3b8e85b1df7a02888d76cf23621786a6ce3d95
Lora Zubić <lamia.lora@gmail.com>
2026-03-20 09:27:59 +0100

    project created

---
+++ WordBreaker/WordBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordBreakerApp.swift
+//  WordBreaker
+//
+//  Created by Lora Zubić on 20.03.2026..
+//
+
+import SwiftUI
+
+@main
+struct WordBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
